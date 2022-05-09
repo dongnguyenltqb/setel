ac75bb3631f38172ba86c74eb826a50c6610ea41
dongnguyenltqb <nhnguhoc@gmail.com>
2022-05-09 19:45:34 +0700

    add build docker image payment

--- payment/README.md
+++ payment/README.md
@@ -1,1 +1,5 @@
+# fastify-rest-api
 
+- Typescript lang, use fastify as a rest api framework
+- Implement follow Clean architecture.
+- Currently able to use for production.
--- payment/src/index.ts
+++ payment/src/index.ts
@@ -2,4 +2,13 @@ import 'dotenv/config'
 import config from './config'
 import Server from './server'
 import logger from './utils/logger'
-new Server(Number(config.server_port)).start().catch((err) => logger.error(err))
+
+async function bootstrap() {
+  try {
+    await new Server(Number(config.server_port)).start()
+  } catch (err) {
+    logger.error(err)
+  }
+}
+
+bootstrap()
